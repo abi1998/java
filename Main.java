9130ff398094f07324735c6a7e270a72d66b6470
abi1998 <43901574+abi1998@users.noreply.github.com>
2018-10-06 14:22:20 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        int n;
+        Scanner s = new Scanner(System.in);
+        System.out.print();
+        n = s.nextInt();
+        if(n > 0)
+        {
+            System.out.println(" positive");
+        }
+        else if(n < 0)
+        {
+            System.out.println(" Negative");
+        }
+        else
+        {
+            System.out.println("zero");
+        }
+    }
+}
