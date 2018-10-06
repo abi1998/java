53500f295ec5ff3d066d16a2de0b8e3462bb3e51
abi1998 <43901574+abi1998@users.noreply.github.com>
2018-10-06 13:57:14 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
-public class Main
+public class main
 {
     public static void main(String[] args) 
     {
         int n;
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter the number you want to check:");
         n = s.nextInt();
         if(n > 0)
         {
